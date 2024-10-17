7116da1ff28819528ca9bf2bacc4437ca60c627a
Adalberto Carvalho Santos Junior <adalbertosantu@gmail.com>
2024-10-17 12:55:38 -0300

    test branch

---
+++ src/context/AuthContext.js
@@ -0,0 +1,9 @@
+/*
+Você
+Para o que serve isso no react js?
+Contextos globais (ex: AuthContext, CartContext)
+Os contextos globais, como AuthContext e CartContext, são super úteis no React JS.
+Eles permitem que você compartilhe dados e funcionalidades entre componentes sem precisar passar props manualmente por todas as camadas da árvore de componentes.
+
+O AuthContext, por exemplo, pode armazenar informações de autenticação do usuário, permitindo que qualquer componente acesse esses dados e verifique se o usuário está logado.
+Já o CartContext pode gerenciar o estado do carrinho de compras em um site de e-commerce, tornando fácil adicionar, remover e listar produtos no carrinho.*/
